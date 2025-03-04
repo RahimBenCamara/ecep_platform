371082798d5b49753103abdf9d5417ab7b6a84fa
RahimBenCamara <rahim.ben.camara@gmail.com>
2025-03-04 17:44:38 +0000

    ajouts

--- frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,9 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import file_picker
+import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
@@ -14,6 +17,9 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
